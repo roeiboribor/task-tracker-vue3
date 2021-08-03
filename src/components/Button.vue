37b4d2bfc3304873c99689820465cbf3e8ab9d7c
Roei Boribor <roeiboribor@gmail.com>
2021-08-03 18:06:30 +0800

    Add New 2 components, Replace favicon, Add Box-Icon cdn

---
+++ src/components/Button.vue
@@ -0,0 +1,9 @@
+<template>
+	<box-icon type="solid" name="plus-square"></box-icon>
+</template>
+
+<script>
+export default defineComponent({
+	name: 'Button',
+});
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'Header',
+	props: {
+		title: {
+			type: String,
+			default: 'Header',
+		},
+	},
+};
+</script>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+</style>
